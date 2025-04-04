3717489307caab0e41adba1951885f471f7f5733
Sumit7420 <sumitchaudhry989@gmail.com>
2025-04-04 11:57:29 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_o5t9HAUbc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '77ra35l4r7a6il1k34vqh7g6sg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
